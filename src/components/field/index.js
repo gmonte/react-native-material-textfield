2f1918b83360b6a71103e0f9ff3ba61d375882b1
Guilherme Monte <gmonte@inf.ufsm.br>
2020-09-07 21:17:52 -0300

    chore: changed text styles proptypes

--- src/components/field/index.js
+++ src/components/field/index.js
@@ -84,9 +84,9 @@ export default class TextField extends PureComponent {
 
     labelOffset: Label.propTypes.offset,
 
-    labelTextStyle: Text.propTypes.style,
-    titleTextStyle: Text.propTypes.style,
-    affixTextStyle: Text.propTypes.style,
+    labelTextStyle: Text.propType,
+    titleTextStyle: Text.propType,
+    affixTextStyle: Text.propType,
 
     tintColor: PropTypes.string,
     textColor: PropTypes.string,
